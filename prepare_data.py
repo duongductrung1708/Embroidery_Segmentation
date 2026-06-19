cc86c4686295499d6faf750a232c623a219399c9
duongductrung1708 <trungyna1708@gmail.com>
2026-06-19 16:51:05 +0700

    ảnh 41

--- prepare_data.py
+++ prepare_data.py
@@ -5,8 +5,8 @@ import os
 # ==========================================
 # CẤU HÌNH GỐC
 # ==========================================
-FILE_NAME = "40.png" # Ảnh gốc vớ vẩn trên mạng (PNG rỗng, JPG nền trắng...)
-OUTPUT_NAME = "40_ready.png" # SẢN PHẨM HOÀN HẢO ĐỂ TRAIN V2 (PHẢI .PNG)
+FILE_NAME = "41.png" # Ảnh gốc vớ vẩn trên mạng (PNG rỗng, JPG nền trắng...)
+OUTPUT_NAME = "41_ready.png" # SẢN PHẨM HOÀN HẢO ĐỂ TRAIN V2 (PHẢI .PNG)
 TARGET_W = 4200
 TARGET_H = 2340
 
