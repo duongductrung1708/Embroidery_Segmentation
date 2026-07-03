8572ed15dbc23329df4131f288bf0e8f766b523c
duongductrung1708 <trungyna1708@gmail.com>
2026-07-03 16:16:46 +0700

    chia lại tập train val

--- scripts/data_prep/convert_single_svg.py
+++ scripts/data_prep/convert_single_svg.py
@@ -525,7 +525,7 @@ def process_single_image(input_path: str, output_path: str = None, use_fal: bool
 
 
 if __name__ == "__main__":
-    INPUT_PATH = "88.png"  
+    INPUT_PATH = "38.png"  
     OUTPUT_PATH = None  
     USE_FAL = True 
     
