31d39eba1c2e0a8fdef343164aaa3b0fd3666e51
duongductrung1708 <trungyna1708@gmail.com>
2026-07-04 16:49:09 +0700

    tiếp tục test và thêm ảnh

--- scripts/data_prep/convert_single_svg.py
+++ scripts/data_prep/convert_single_svg.py
@@ -525,7 +525,7 @@ def process_single_image(input_path: str, output_path: str = None, use_fal: bool
 
 
 if __name__ == "__main__":
-    INPUT_PATH = "113.png"  
+    INPUT_PATH = "118.png"  
     OUTPUT_PATH = None  
     USE_FAL = True 
     
