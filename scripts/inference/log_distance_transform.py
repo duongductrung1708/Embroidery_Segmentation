02598692b8dc99c49ae5da40054b07da7d4c2dea
duongductrung1708 <trungyna1708@gmail.com>
2026-07-17 17:09:34 +0700

    .

--- scripts/inference/log_distance_transform.py
+++ scripts/inference/log_distance_transform.py
@@ -219,4 +219,4 @@ def debug_shape_thickness_fixed(image_path, physical_width_mm=80.0, threshold_ra
     plt.show()
 
 # Test thu
-debug_shape_thickness_fixed('data/opencv_test/predictions/155_pred.png', physical_width_mm=80.0, threshold_ratio=6.0)
+debug_shape_thickness_fixed('data/opencv_test/predictions/test_pred.png', physical_width_mm=80.0, threshold_ratio=6.0)
